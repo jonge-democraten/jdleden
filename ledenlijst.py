f941012c15ba5c37debbe0b475a4ade3f1806e90
Jasper Jongmans <jasper.jongmans@jongedemocraten.nl>
2012-02-12 17:32:56 +0100

    Implement proper logging
    Rewrite print-statements to logger
    Output to console, debug.log and info.log

--- ledenlijst.py
+++ ledenlijst.py
@@ -10,8 +10,10 @@ import time
 import hashlib
 import ConfigParser
 from optparse import OptionParser
+import logging
 
-now = time.strftime("%s")
+now = time.strftime("%s")	# epoch-time
+nowhuman = time.strftime("%F %T")	# human readable time
 
 # trick to read config from same dir as actual script, even when called via symlink
 scriptdir = os.path.dirname(os.path.realpath(__file__))
@@ -20,6 +22,33 @@ config.read(os.path.join(scriptdir, "ledenlijst.cfg"))
 dbcfg = {}
 for o, v in config.items("database"):
 	dbcfg[o] = v
+
+# set up logging
+logdir = os.path.join(scriptdir, "log", nowhuman)
+# make directory if needed
+try:
+	os.makedirs(logdir, 0700)
+except IOError as e:
+	if e.errno == errno.EEXIST:
+		pass
+	else:
+		raise
+# log to console, debug.log and info.log
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+fhd = logging.FileHandler(os.path.join(logdir, "debug.log"))
+fhd.setLevel(logging.DEBUG)
+fhi = logging.FileHandler(os.path.join(logdir, "info.log"))
+fhi.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
+ch.setFormatter(formatter)
+fhd.setFormatter(formatter)
+fhi.setFormatter(formatter)
+logger.addHandler(ch)
+logger.addHandler(fhd)
+logger.addHandler(fhi)
 
 # Geef alle belangrijke kolommen een naam
 LIDNUMMER = 0
@@ -89,7 +118,7 @@ def read_xls(f):
 #			leden[int(row[LIDNUMMER].value)][3] = xlrd.xldate_as_tuple(leden[int(row[LIDNUMMER].value)][3], 0)
 #			leden[int(row[LIDNUMMER].value)][6] = xlrd.xldate_as_tuple(leden[int(row[LIDNUMMER].value)][6], 0)
 		except:
-			print "geen voor- of achternaam"
+			logger.warning("geen voor- of achternaam")
 	return leden
 
 def write_csv(f, members):
@@ -142,13 +171,14 @@ def split_by_department(members):
 
 def dosql(c, sql, value):
 	# DRY - use one function and don't repeat code
-	if options.dryrun:
-		print (sql % value).encode("utf-8")
-	else:
-		c.execute(sql, value)
-	if options.debug:
-		for msg in c.messages:
-			print "DEBUG: ", msg
+	logger.debug((sql % value).encode("utf-8"))
+	if not options.dryrun:
+		try:
+			c.execute(sql, value)
+		except:
+			logger.error("Error executing previous query")
+	for msg in c.messages:
+		logger.debug(msg)
 			
 
 if __name__ == "__main__":
@@ -158,8 +188,6 @@ Usage: %prog [options] arguments
   in regular and jNews-only-mode, arguments is 2 files: old.xls new.xls
   in Excel-only-mode, arguments is 1 file: new.xls"""
 	parser = OptionParser(usage)
-	parser.add_option("-v", "--verbose", action="store_true", dest="debug",
-					help="produce debug output")
 	parser.add_option("-n", "--dryrun", action="store_true", dest="dryrun",
 					help="don't execute any SQL")
 	parser.add_option("-j", "--jnews", action="store_true", dest="only_jnews",
@@ -178,7 +206,7 @@ Usage: %prog [options] arguments
 	else:
 		if len(args) != 2:
 			parser.error("need 2 arguments: old.xls new.xls")
-		print "Verifying sanity...",
+		logger.info("Verifying sanity of input files")
 		csumfile = os.path.join(scriptdir, "checksum.txt")
 		oldfile = args[0]
 		newfile = args[1]
@@ -191,7 +219,7 @@ Usage: %prog [options] arguments
 		except IOError as e:
 			# als nog geen checksum.txt, doe alsof er oldsha in stond
 			if e.errno == errno.ENOENT:
-				print "Writing checksum...",
+				logger.warning("Will create new checksum.txt")
 				storedsha = oldsha
 			else:
 				raise
@@ -202,48 +230,26 @@ Usage: %prog [options] arguments
 			with open(csumfile,"w") as f:
 				# append newline to mimic sha512sum behaviour
 				f.write(newsha+"\n")
-				print "Sane"
+				logger.info("Input files are sane")
 		else:
-			print "Wrong old.xls"
+			logger.critical("Wrong old.xls")
 			sys.exit(1)
 	
 	# FIXME xlwt code should go somewhere around here (before SQL-code)
 	if not options.only_jnews:
-		print "placeholder xlwt"
+		logger.info("placeholder xlwt")
 		
 	if not options.only_excel:
-		print "Reading member lists...",
+		logger.info("Reading member lists...")
 		old = read_xls(oldfile)
 		new = read_xls(newfile)
-		print "Done"
+		logger.info("Reading complete")
 	
-		print "Computing changes...",
+		logger.info("Computing changes...")
 		plus, min = get_new_and_former_members(old, new)
 		changed = get_changed_members(old, new)
-		print "Done"
-	
-		print "Writing cvs files:"
 		plus_split = split_by_department(plus)
-		min_split = split_by_department(min)
-		changed_split = split_by_department(changed)
-		directory = time.strftime("historie/%F %T")
-		# maak directories en negeer als al bestaat
-		try:
-			os.makedirs(directory, 0700)
-		except IOError as e:
-			if e.errno == errno.EEXIST:
-				pass
-			else:
-				raise
-		for d in plus_split.keys():
-			print "%s-plus.csv\t%d" % (d, len(plus_split[d]))
-			write_csv("%s/%s-plus.csv" % (directory,d), plus_split[d])
-		for d in min_split.keys():
-			print "%s-min.csv\t%d" % (d, len(min_split[d]))
-			write_csv("%s/%s-min.csv" % (directory,d), min_split[d])
-		for d in changed_split.keys():
-			print "%s-upd.csv\t%d" % (d, len(changed_split[d]))
-			write_csv("%s/%s-upd.csv" % (directory,d), changed_split[d])
+		logger.info("Computing complete")
 	
 		# In de jnews tables gebruik ik het email adres als identifier voor de persoon.
 		# De lid id kan niet gebruikt worden vanwege mogelijke collisions door 2 
@@ -252,13 +258,15 @@ Usage: %prog [options] arguments
 		c = db.cursor()
 	
 		# remove old members
+		logger.info("Removing members...")
 		for m in min:
 			value = (now, min[m][EMAIL])
 			sql = "UPDATE IGNORE j16_jnews_listssubscribers SET unsubdate=%s, unsubscribe=1 WHERE subscriber_id = (SELECT id FROM j16_jnews_subscribers WHERE email=%s)"
 			dosql(c, sql, value)
-		print "Removing complete"
+		logger.info("Removing complete")
 		
 		# update changed members
+		logger.info("Updating changed members...")
 		moved = {}
 		for id in changed.keys():
 			if (changed[id][NAAM] != old[id][NAAM] or changed[id][EMAIL] != old[id][EMAIL]):
@@ -273,54 +281,48 @@ Usage: %prog [options] arguments
 				if (newdept != olddept):
 					moved[id] = changed[id]
 		moved_split = split_by_department(moved)
-		print "Changes complete"	
+		logger.info("Changes complete")
 	
 		# Add new members	
+		logger.info("Adding new members...")
 		for d in plus_split.keys():
 			for id in plus_split[d].keys():
 				value = (plus_split[d][id][NAAM], plus_split[d][id][EMAIL], 1, now)
 				sql = "INSERT INTO j16_jnews_subscribers (name, email, confirmed, subscribe_date) VALUES (%s, %s, %s, %s) ON DUPLICATE KEY UPDATE id=id"
 				dosql(c, sql, value)
-		print "Adding complete"
+		logger.info("Adding complete")
 		
 		# Add the new members to their department
+		logger.info("Subscribing new members to lists...")
 		for d in plus_split.keys():
 	#		values = [(db.escape_string(plus_split[d][id][EMAIL]), db.escape_string("Digizine")) for id in plus_split[d].keys()]
 			values = [(db.escape_string(plus_split[d][id][EMAIL]), db.escape_string("Digizine"), db.escape_string("Nieuwsbrief "+d)) for id in plus_split[d].keys()]
 			for v in values:
-				try:
-					value = (v[0], v[1])
+				value = (v[0], v[1])
+				sql = "INSERT INTO j16_jnews_listssubscribers (subscriber_id, list_id) VALUES ((SELECT id FROM j16_jnews_subscribers WHERE email=%s LIMIT 1), (SELECT id FROM j16_jnews_lists WHERE list_name=%s)) ON DUPLICATE KEY UPDATE list_id = list_id"
+				dosql(c, sql, value)
+				if v[2] != "Nieuwsbrief Buitenland":
+					value = (v[0], v[2])
 					sql = "INSERT INTO j16_jnews_listssubscribers (subscriber_id, list_id) VALUES ((SELECT id FROM j16_jnews_subscribers WHERE email=%s LIMIT 1), (SELECT id FROM j16_jnews_lists WHERE list_name=%s)) ON DUPLICATE KEY UPDATE list_id = list_id"
 					dosql(c, sql, value)
-					if v[2] != "Nieuwsbrief Buitenland":
-						value = (v[0], v[2])
-						sql = "INSERT INTO j16_jnews_listssubscribers (subscriber_id, list_id) VALUES ((SELECT id FROM j16_jnews_subscribers WHERE email=%s LIMIT 1), (SELECT id FROM j16_jnews_lists WHERE list_name=%s)) ON DUPLICATE KEY UPDATE list_id = list_id"
-						dosql(c, sql, value)
-				except:
-					print "Error executing \"%s\"" %sql
-					print "Handmatige interventie voor bovenstaande query is nodig"
+		logger.info("Subscribing complete")
 		# Unsubscribe moved members from old department and subscribe to new department
 		# FIXME DRY
+		logger.info("Moving members to new departments...")
 		for d in moved_split.keys():
 			values = [(db.escape_string(moved_split[d][id][EMAIL]), db.escape_string("Nieuwsbrief "+d)) for id in moved_split[d].keys()]
 			for v in values:
-				try:
-					# unsubscribe old
-					olddept = find_department(parse_postcode(old[id][POSTCODE]))
-					oldlist = "Nieuwsbrief "+olddept
-					value = (now, oldlist, v[0])
-					sql = "UPDATE IGNORE j16_jnews_listssubscribers SET unsubdate=%s, unsubscribe=1 WHERE list_id IN (SELECT id FROM j16_jnews_lists WHERE list_name=%s) AND subscriber_id = (SELECT id FROM j16_jnews_subscribers WHERE email=%s)"
+				# unsubscribe old
+				olddept = find_department(parse_postcode(old[id][POSTCODE]))
+				oldlist = "Nieuwsbrief "+olddept
+				value = (now, oldlist, v[0])
+				sql = "UPDATE IGNORE j16_jnews_listssubscribers SET unsubdate=%s, unsubscribe=1 WHERE list_id IN (SELECT id FROM j16_jnews_lists WHERE list_name=%s) AND subscriber_id = (SELECT id FROM j16_jnews_subscribers WHERE email=%s)"
+				dosql(c, sql, value)
+				# subscribe new
+				if v[1] != "Nieuwsbrief Buitenland":
+					value = (v[0], v[1], now)
+					sql = "INSERT INTO j16_jnews_listssubscribers (subscriber_id, list_id, subdate) VALUES ((SELECT id FROM j16_jnews_subscribers WHERE email=%s LIMIT 1), (SELECT id FROM j16_jnews_lists WHERE list_name=%s), %s) ON DUPLICATE KEY UPDATE list_id = list_id"
 					dosql(c, sql, value)
-				except:
-					print "Error executing: %s" % (sql % value)
-				try:
-					# subscribe new
-					if v[1] != "Nieuwsbrief Buitenland":
-						value = (v[0], v[1], now)
-						sql = "INSERT INTO j16_jnews_listssubscribers (subscriber_id, list_id, subdate) VALUES ((SELECT id FROM j16_jnews_subscribers WHERE email=%s LIMIT 1), (SELECT id FROM j16_jnews_lists WHERE list_name=%s), %s) ON DUPLICATE KEY UPDATE list_id = list_id"
-						dosql(c, sql, value)
-				except:
-					print "Error executing: %s" % (sql % value)
-		print "Added to mailinglists"	
+		logger.info("Moving complete")
 
 
