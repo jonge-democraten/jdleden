60c09275a6881b253ca69bc594323ba7e41f92ed
Bart Römgens <bart.romgens@gmail.com>
2016-11-19 14:06:05 +0100

    improve local_settings example

--- jdleden/local_settings_example.py
+++ jdleden/local_settings_example.py
@@ -6,8 +6,19 @@ LDAP_DN = 'cn=writeuser,ou=sysUsers,dc=jd,dc=nl'
 SECRET_KEY = ''
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'dev.db',
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.sqlite3",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "dev.db",
+        # Not used with sqlite3.
+        "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
     }
 }
+
